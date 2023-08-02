634532dba58da3669bd35e68f5311b6b7ae088a1
ChristianRodriguez <chrirodriguez@csumb.edu>
2023-08-01 23:04:22 -0700

    Assignment 5 re-up

--- cst438_gradebook/src/test/java/com/cst438/EndToEndTestCreateAssignment.java
+++ cst438_gradebook/src/test/java/com/cst438/EndToEndTestCreateAssignment.java
@@ -44,7 +44,7 @@ public class EndToEndTestCreateAssignment {
 
     @Test
     public void testAddAssignment() throws Exception {
-       // Database setup: create courses
+       // Database setup: create course
        Course course = new Course();
        course.setCourse_id(TEST_COURSE_ID);
        course.setInstructor(TEST_INSTRUCTOR_EMAIL);
